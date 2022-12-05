a98b21c34a1781c8ac3d99de73bdf72925150a06
ClairaM <claira.monroe@trojans.dsu.edu>
2022-12-04 19:20:44 -0600

    Modified the home page: changed colors, navagation bar, and added a button. Also added a models file with an Ingredient.cs. Added an Ingredient class in the Ingredient.cs file.

---
+++ Models/Ingredient.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace MadisonPizzeria.Models
+{
+    public class Ingredient
+    {
+        public string Size { get; set; }
+        public string Crust { get; set; }
+        public string Sauce { get; set; }
+        public string Cheese { get; set; }
+		public string Meat { get; set; }
+        public string Topping { get; set; }
+		public bool Extra { get; set; }
+        public float Cost { get; set; }
+        
+    }
+}
